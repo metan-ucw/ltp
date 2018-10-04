afe9dac5cec83497bbdcd605774466c7f36b7511
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2018-10-04 23:16:35 +0200

    runltp_ng(ssh): Use differnt user then root
    If the root access is forbidden by ssh, you should specify
    a different user. To gain root, we will use "sudo su".
    This is common for public cloud instances.

--- tools/runltp-ng/backend.pm
+++ tools/runltp-ng/backend.pm
@@ -350,6 +350,7 @@ sub ssh_start
 {
 	my ($self) = @_;
 	my $host = $self->{'ssh_host'};
+	my $user = $self->{'ssh_user'};
 
 	msg("Waiting for $host to appear\n");
 	while (system("ping -c 1 $host >/dev/null 2>&1")) {
@@ -361,7 +362,7 @@ sub ssh_start
 		sleep(1);
 	}
 
-	my $cmdline = "export TERM=dumb; script -f -c 'ssh root@" . "$host' /dev/null";
+	my $cmdline = "export TERM=dumb; script -f -c 'ssh $user\@$host' /dev/null";
 
 	msg("Starting ssh: $cmdline\n");
 
@@ -383,13 +384,21 @@ sub ssh_start
 	run_string($self, "$self->{'root_password'}");
 	sleep(1); #hack wait for prompt
 	wait_prompt($self);
+	if ($user ne 'root') {
+		run_string($self, "sudo su");
+		wait_prompt($self);
+	}
 }
 
 sub ssh_stop
 {
 	my ($self) = @_;
+	my $user = $self->{'ssh_user'}; 
 
 	run_string($self, "exit");
+	if ($user ne 'root') {
+		run_string($self, "exit");
+	}
 
 	waitpid($self->{'pid'}, 0);
 }
@@ -408,6 +417,7 @@ sub ssh_force_stop
 my $ssh_params = [
 	['password', 'root_password', "Remote machine root password"],
 	['host', 'ssh_host', "Remote machine hostname or IP"],
+	['user', 'ssh_user', "Remote user, if other then root use sudo to get root"],
 	['serial_relay_port', 'serial_relay_port', "Serial relay poor man's reset dongle port"],
 ];
 
@@ -419,6 +429,7 @@ sub ssh_init
 
 	die("ssh:host must be set!") unless defined($backend{'ssh_host'});
 	die("ssh:root_password must be set!") unless defined($backend{'root_password'});
+	$backend{'ssh_user'} //= 'root';
 
 	$backend{'start'} = \&ssh_start;
 	$backend{'stop'} = \&ssh_stop;
